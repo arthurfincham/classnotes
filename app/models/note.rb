7253837e1e4f3d1748b526393271f82cdfffff48
Arthur Fincham <arthurfincham@me.com>
2021-08-28 16:18:37 +0100

    one2many database now working

---
+++ app/models/note.rb
@@ -0,0 +1,4 @@
+class Note < ApplicationRecord
+  belongs_to :user
+  validates :user_id, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_142412) do
+ActiveRecord::Schema.define(version: 2021_08_28_150334) do
+
+  create_table "notes", force: :cascade do |t|
+    t.string "title"
+    t.date "date"
+    t.string "instructor"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
