c9658da64bf27efe58e338473a2cb301717701ab
Arthur Fincham <arthurfincham@me.com>
2021-08-29 09:37:12 +0100

    creation and redirect from create working

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,3 +1,28 @@
 class NotesController < ApplicationController
 
+  def index
+    @notes = Note.all
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+   # render plain: params[:note].inspect\
+   @note = Note.new(note_params)
+   @note.user = User.first
+   if @note.save
+    flash[:notice] = "Note was successfully created"
+    redirect_to notes_path
+   else
+    render 'new'
+   end 
+  end
+
+  private
+    def note_params
+     params.require(:note).permit(:user_id, :title, :date, :instructor, :description)
+    end
+
 end
