f948a46e4e23499e59df50988bf7148fdbf8e138
Arthur Fincham <arthurfincham@me.com>
2021-09-01 09:12:46 +0100

    successful move from sqlite to postgres

--- db/migrate/20210830145542_remove_tag_tables.rb
+++
@@ -1,6 +0,0 @@
-class RemoveTagTables < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :tags
-    drop_table :taggings
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_30_145542) do
+ActiveRecord::Schema.define(version: 2021_08_29_171654) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "notes", force: :cascade do |t|
     t.string "title"
