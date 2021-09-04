2c7cb89beaa13f87fc483cd18c18969c8ac165bf
Arthur Fincham <arthurfincham@me.com>
2021-09-04 15:19:57 +0100

    controller slimming

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,8 @@ class HomeController < ApplicationController
       redirect_to new_user_session_path
     end
     if user_signed_in?
-      @recent_notes = current_user.notes.last(5)
-      @user_notes = current_user.notes.last(5)
       @current_user = current_user
+      @user_notes = current_user.notes.last(5)
       @guide_notes = Guide.all
     end
   end
