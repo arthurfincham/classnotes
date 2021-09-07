b2916e22e0ff71911f98da2f3ff64c016522da5a
Arthur Fincham <arthurfincham@me.com>
2021-09-07 12:43:18 +0100

    split partials, added top tags

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,7 @@ class HomeController < ApplicationController
       @current_user = current_user
       @user_notes = current_user.notes.last(5)
       @guide_notes = Guide.all
+      @tag_counts = current_user.notes.tag_counts_on(:tags).limit(6)
     end
   end
 end
