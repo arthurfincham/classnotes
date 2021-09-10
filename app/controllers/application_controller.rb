7a9feaba53652072a06d17ef60fcc8a1381b303c
Arthur Fincham <arthurfincham@me.com>
2021-09-10 15:58:49 +0100

    added routing-master which gives info for each page - shows controller, action, path and partials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   helper_method :mobile?
+  before_action :log_partial_events
 
-   
+  def log_partial_events
+    @partial_events = []
+    ActiveSupport::Notifications.subscribe("render_partial.action_view") do |event_name, start_at, end_at, id, payload|
+    @partial_events << payload[:identifier]
+  end
+end
 
 def after_sign_in_path_for(resource_or_scope)
   home_index_path
