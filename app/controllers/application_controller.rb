ed97b7992c3478b7b493da1bccd519b7a8fbd24a
Arthur Fincham <arthurfincham@me.com>
2021-09-11 09:46:45 +0100

    format application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,31 +1,30 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-  helper_method :mobile?
   before_action :log_partial_events
+  helper_method :mobile?
 
   def log_partial_events
     @partial_events = []
     ActiveSupport::Notifications.subscribe("render_partial.action_view") do |event_name, start_at, end_at, id, payload|
     @partial_events << payload[:identifier]
+    end
   end
-end
 
-def after_sign_in_path_for(resource_or_scope)
-  home_index_path
-end
+  def after_sign_in_path_for(resource_or_scope)
+    home_index_path
+  end
 
-def after_sign_out_path_for(resource_or_scope)
-  new_user_session_path
-end
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_session_path
+  end
 
   def index
     @current_user = current_user
   end
 
-
   def edit
-    @user = User.find(params[:id])
     @current_user = current_user
+    @user = User.find(params[:id])
   end 
 
   protected
@@ -38,7 +37,8 @@ end
 
   private
 
-   def mobile? # has to be in here because it has access to "request"
-      request.user_agent =~ /\b(Android|iPhone|iPad|Windows Phone|Opera Mobi|Kindle|BackBerry|PlayBook)\b/i
-   end
+  def mobile? # has to be in here because it has access to "request"
+    request.user_agent =~ /\b(Android|iPhone|iPad|Windows Phone|Opera Mobi|Kindle|BackBerry|PlayBook)\b/i
+  end
+
 end
